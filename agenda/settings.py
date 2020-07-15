53e5933daa66376155528c07c183ef62218d8e87
raul-gomes <rsgomes86@hotmail.com>
2020-07-15 14:27:24 -0300

    filtros
    Adicionado alguns filtros na agenda.

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=3!p-355tycov17n2fjs-l71!_x^rr60fljd98s9nz0gf!ahv_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
